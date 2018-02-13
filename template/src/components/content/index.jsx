989dc14ee76938250d3f13406759b1308b1a46af
justin lee <yuminjustin@163.com>
2018-02-13 14:47:13 +0800

    Update index.jsx

--- template/src/components/content/index.jsx
+++ template/src/components/content/index.jsx
@@ -1,35 +1,45 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router'
 import { observer } from 'mobx-react';
-import Bundle from '../../bundle'
+import Loadable from 'react-loadable';
 
 import { Layout } from 'antd'
 const { Content } = Layout
 
-const Hello = (props) => (
-  <Bundle load={() => import('../hello')}>
-    {(Hello) => <Hello {...props} />}
-  </Bundle>
-);
+function Loading(props) {
+  if (props.error) {
+    return <div>Error!</div>;
+  } else if (props.pastDelay) {
+    return <div>Loading...</div>;
+  } else {
+    return null;
+  }
+}
 
-const Demo = (props) => (
-  <Bundle load={() => import('../demo')}>
-    {(Demo) => <Demo {...props} />}
-  </Bundle>
-);
+const Hello = Loadable({
+  loader: () => import('../hello'),
+  loading: Loading,
+  delay: 300
+});
 
-const Test = (props) => (
-  <Bundle load={() => import('../test')}>
-    {(Test) => <Test {...props} />}
-  </Bundle>
-);
+const Demo = Loadable({
+  loader: () => import('../demo'),
+  loading: Loading,
+  delay: 300
+});
+
+const Test = Loadable({
+  loader: () => import('../test'),
+  loading: Loading,
+  delay: 300
+});
 
 // 404
-const NoMatch = (props) => (
-  <Bundle load={() => import('../error')}>
-    {(NoMatch) => <NoMatch {...props} />}
-  </Bundle>
-);
+const NoMatch = Loadable({
+  loader: () => import('../error'),
+  loading: Loading,
+  delay: 300
+});
 
 
 export default class Contents extends Component {
@@ -40,7 +50,7 @@ export default class Contents extends Component {
   render() {
 
     return (
-      <Content className="_content" style=\{{ height: this.state.height }}>
+      <Content className="_content" style={{ height: this.state.height }}>
         <Switch>
           <Route exact path="/" component={Hello} />
           <Route exact path="/demo" component={Demo} />
