014a88c55dd84a7426e08e91a215c2851217cd68
justin lee <yuminjustin@163.com>
2018-12-27 15:22:59 +0800

    Update index.jsx

--- template/src/components/demo/index.jsx
+++ template/src/components/demo/index.jsx
@@ -4,7 +4,7 @@ import { inject, observer } from 'mobx-react';
 import Heads from './head'
 import List from './list'
 import Footer from './foot'
-
+import style from 'B/assets/css/style.css';
 @inject('todos')
 @inject('types')
 @observer
@@ -13,10 +13,10 @@ export default class Demo extends Component {
     render() {
         let { props } = this;
         return (
-            <div className="mainbox m oh">
+            <div className={style.mainbox}>
                 <h1>Mobx todo</h1>
                 <Heads todos={props.todos} />
-                <div className="c oh listbox">
+                <div className={style.listbox}>
                     <List todos={props.todos} types={props.types} />
                 </div>
                 <Footer todos={props.todos} types={props.types} />
