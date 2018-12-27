d4f4a7d05f63883f253a2fa4c7c4ddf5d8a08327
justin lee <yuminjustin@163.com>
2018-12-27 15:22:19 +0800

    Update foot.jsx

--- template/src/components/demo/foot.jsx
+++ template/src/components/demo/foot.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-
+import style from 'B/assets/css/style.css';
 @observer
 export default class Footer extends Component {
     render() {
@@ -10,14 +10,14 @@ export default class Footer extends Component {
             c3 = types.type == 2 ? 'active' : '';
 
         return (
-            <div className="last_info">
-                <p className="l info">{todos.noChoose}项未选择</p>
-                <p className="l choose_btn">
+            <div className={style.last_info}>
+                <p className={style.info}>{todos.noChoose}项未选择</p>
+                <p className={style.choose_btn}>
                     <span className={c1} onClick={this.showAll.bind(this)}>所有</span>
                     <span className={c2} onClick={this.showNone.bind(this)}>未选</span>
                     <span className={c3} onClick={this.showChoose.bind(this)}>已选</span>
                 </p>
-                <button className="del r" style=\{{ display: todos.active ? "initial" : "none" }} onClick={this.deleteHandler.bind(this)}>删除选中项</button>
+                <button className={style.del} style=\{{ display: todos.active ? "initial" : "none" }} onClick={this.deleteHandler.bind(this)}>删除选中项</button>
             </div>
         )
     }
