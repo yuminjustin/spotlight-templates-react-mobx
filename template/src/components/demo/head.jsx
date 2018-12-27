f1edf73c653ccdd949162db1cf8d36dde978d9f0
justin lee <yuminjustin@163.com>
2018-12-27 15:23:34 +0800

    Update head.jsx

--- template/src/components/demo/head.jsx
+++ template/src/components/demo/head.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import {observer} from 'mobx-react';
-
+import style from 'B/assets/css/style.css';
 @observer
 export default class Heads extends Component {
 
     render() {
         let {todos} = this.props;
         return (
-            <div className="c addbox">
+            <div className={style.addbox}>
                 <label>
-                    <input type="checkbox" className="l db" ref="all" onChange={this.chooseHandler.bind(this)} checked={todos.chooseAll}/> 全选
+                    <input type="checkbox" className={style.dbl} ref="all" onChange={this.chooseHandler.bind(this)} checked={todos.chooseAll}/> 全选
                 </label>
                 <input type="text" placeholder="输入需要添加的文字" ref="ipt"/>
                 <button onClick={this.clickHandler.bind(this)}>添加</button>
